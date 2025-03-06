475b33c972d9d35e473ee8131b301c2b72d7945e
Belayne <37399395+Belayne@users.noreply.github.com>
2025-03-06 15:14:01 +0100

    feat: add upload post controller

--- controllers/uploadController.ts
+++ controllers/uploadController.ts
@@ -1,9 +1,32 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    console.log("file: ", file);
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    console.log("file: ", file);
+    const uniqueSuffix = Date.now() + "-";
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
 const uploadController = {
   showUploadPage: (req, res) => {
     if (!res.locals.user) {
       return res.redirect("/");
     }
     res.render("uploadPage");
+  },
+
+  uploadFile: upload.single("file"),
+
+  handleUpload: (req, res) => {
+    console.log(req.file);
+    res.redirect("/");
   },
 };
 
--- routes/uploadRouter.ts
+++ routes/uploadRouter.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import uploadController from "../controllers/uploadController";
-
 const uploadRouter = Router();
 
 uploadRouter.get("/upload", uploadController.showUploadPage);
+uploadRouter.post(
+  "/upload",
+  uploadController.uploadFile,
+  uploadController.handleUpload
+);
 
 export default uploadRouter;
