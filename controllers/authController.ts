aa21a1390c9b357059944013d0bf84f8bd38d408
Belayne <37399395+Belayne@users.noreply.github.com>
2025-03-06 10:23:30 +0100

    feat: add signup user creation in auth controller

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,3 +1,33 @@
+import { body } from "express-validator";
+import { validationResult } from "express-validator";
+import client from "../prisma/prismaClient";
+import bcryptjs from "bcryptjs";
+
+const signupValidations = [
+  body("username")
+    .trim()
+    .isAlphanumeric()
+    .withMessage("Username can only contain letters and numbers.")
+    .custom(async (username) => {
+      const user = await client.user.findFirst({
+        where: { username },
+      });
+      if (user) {
+        throw new Error("Username already in use");
+      }
+    }),
+
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must have at least 8 characters."),
+  body("confirmPassowrd").custom((confirm, { req }) => {
+    if (confirm === req.body.password) {
+      throw new Error("Passwords do not match.");
+    }
+    return true;
+  }),
+];
+
 const authController = {
   showLoginPage: (req, res) => {
     if (res.locals.user) {
@@ -15,7 +45,27 @@ const authController = {
     res.render("signup");
   },
 
-  createUser: (req, res) => {},
+  createUser: async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render("signup", { errors: errors.array() });
+    }
+
+    const { username, password } = req.body;
+
+    try {
+      const hashedPassword = await bcryptjs.hash(password, 10);
+      await client.user.create({
+        data: {
+          username,
+          password: hashedPassword,
+        },
+      });
+      res.redirect("/");
+    } catch (error) {
+      next(error);
+    }
+  },
 };
 
-export default authController;
+export { authController, signupValidations };
--- routes/authRouter.ts
+++ routes/authRouter.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import authController from "../controllers/authController";
+import {
+  authController,
+  signupValidations,
+} from "../controllers/authController";
 
 const authRouter = Router();
 
 authRouter.get("/login", authController.showLoginPage);
 authRouter.get("/signup", authController.showSignupPage);
+authRouter.post("/signup", signupValidations, authController.createUser);
 
 export default authRouter;
